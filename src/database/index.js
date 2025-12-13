d49e71aa6bdb48caa5af43502c2faf970547c8e1
Samuel-Galassi <sgcontatodireto@gmail.com>
2025-12-13 20:19:11 -0300

    included deploy files and configuration

--- src/database/index.js
+++ src/database/index.js
@@ -20,7 +20,9 @@ class Database {
                 (model) => model.associate && model.associate(this.connection.models));
     }
     mongo() {
-        this.mongooseConnection = mongoose.connect('mongodb://localhost:27017/DevBurguer')
+        this.mongooseConnection = mongoose.connect(
+            process.env.MONGO_URL,
+        );
     }
 }
 export default new Database();
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import app from "./app.js";
 import './database/index.js';
 
-app.listen(3001, () => {
+
+const port = process.env.PORT
+app.listen(port, () => {
     console.log('Server running on port 3001');
 });
