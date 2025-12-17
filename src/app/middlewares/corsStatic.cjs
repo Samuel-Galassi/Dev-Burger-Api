647b0e04c768f7712b903811ae7c8051b8a1157b
Samuel-Galassi <sgcontatodireto@gmail.com>
2025-12-17 01:14:06 -0300

    changed corsStatic.cjs

--- src/app/middlewares/corsStatic.cjs
+++ src/app/middlewares/corsStatic.cjs
@@ -2,6 +2,7 @@
 
 function corsStatic(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', process.env.URL_VERCEL);
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin'); // Essencial para imagens
   next();
 }
 
