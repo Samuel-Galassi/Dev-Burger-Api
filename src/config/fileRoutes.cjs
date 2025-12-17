bde1422127eb577167ab3d310cc0ecb560daab5f
Samuel-Galassi <sgcontatodireto@gmail.com>
2025-12-17 01:28:25 -0300

    att

--- src/config/fileRoutes.cjs
+++ src/config/fileRoutes.cjs
@@ -5,16 +5,11 @@ const uploadPath = resolve(__dirname, '..', '..', 'uploads');
 
 const router = express.Router();
 
-// Middleware CORS para todas as rotas
-router.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.URL_VERCEL);
-  next();
-});
-
-// Servir arquivos estáticos com cabeçalho específico para CORB
+// Servir arquivos estáticos com CORS correto
 router.use('/product-file', express.static(uploadPath, {
   setHeaders: (res, path, stat) => {
-    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin'); 
+    res.setHeader('Access-Control-Allow-Origin', process.env.URL_VERCEL); // libera frontend
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin'); // essencial para imagens
   }
 }));
 
