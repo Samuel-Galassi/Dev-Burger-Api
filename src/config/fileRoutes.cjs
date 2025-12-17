9ff075c5f7e871f0aefe8fb76a5e237e3e3707ca
Samuel-Galassi <sgcontatodireto@gmail.com>
2025-12-17 01:19:58 -0300

    deletando corsStatic.cjs

--- src/config/fileRoutes.cjs
+++ src/config/fileRoutes.cjs
@@ -1,12 +1,21 @@
 const { resolve } = require('node:path');
 const express = require('express');
-const corsStatic = require('../app/middlewares/corsStatic.cjs');
 
 const uploadPath = resolve(__dirname, '..', '..', 'uploads');
 
 const router = express.Router();
 
-router.use(corsStatic); // aplica CORS
-router.use(express.static(uploadPath));
+// Middleware CORS para todas as rotas
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.URL_VERCEL);
+  next();
+});
+
+// Servir arquivos estáticos com cabeçalho específico para CORB
+router.use('/product-file', express.static(uploadPath, {
+  setHeaders: (res, path, stat) => {
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin'); 
+  }
+}));
 
 module.exports = router;
